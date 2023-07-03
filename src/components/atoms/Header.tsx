fb03fbd729af4fa1050ed9d7f51b4f9efc4dfe2a
kurone-kito <krone@kit.black>
2023-07-03 23:08:14 +0900

    feat: added the header component

---
+++ src/components/atoms/Header.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+export const Header: FC = () => (
+  <header className="text-left flex-grow-0">
+    <h1 className="font-extralight text-xl">
+      ⚔️ Dream Duels: The Battle for Oneiron
+    </h1>
+  </header>
+);
